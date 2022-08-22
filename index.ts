99304aa425ecd61606cfd1052e663818456f96c4
safak <syavsan@yukatech.com>
2022-08-22 15:52:31 +0300

    feat: dockerized.

--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response, Router } from "express";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
-});
-
+const router: Router = express.Router();
+const port: string | undefined = process.env.API_SERVER_PORT;
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
+app.use(router);
+router.get("/", (_req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
 });
